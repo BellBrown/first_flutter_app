206d133b03ec55b6a14be664f1326b8b8c2f4e02
BellBrown <94917218+BellBrown@users.noreply.github.com>
2021-11-29 13:51:04 +0000

    added a card

--- lib/main.dart
+++ lib/main.dart
@@ -31,8 +31,8 @@ class BellHomePage extends StatelessWidget {
        Icon(Icons.search_off_outlined),
        Icon(Icons.more_vert)     ]
      ),
-     body:  Center(
-       child: ListTile(leading: const CircleAvatar(radius: 30,backgroundImage:AssetImage("assets/images/profile0.jfif"),),
+     body:  Column(
+       children:[ ListTile(leading: const CircleAvatar(radius: 30,backgroundImage:AssetImage("assets/images/profile0.jfif"),),
        title: const Text("Rexford"),
        // ignore: prefer_const_literals_to_create_immutables
        subtitle:Row(children: [const Icon(Icons.done_all),
@@ -42,9 +42,24 @@ class BellHomePage extends StatelessWidget {
        trailing:  Column(
          children: [Text("3:30"),Icon(Icons.notifications)],
        ) ,
-         )
+         ),
+         //this is my listTile
+         Card(color: Colors.pink,
+            child: ListTile(
+              leading: CircleAvatar(radius: 35,backgroundImage: AssetImage("assets/images/profile1.jfif"),
+                   ),
+                   title: Text("Christabel "),
+                   subtitle: Text("Brown am done"),
+          
+                   trailing: Column(children: const [Text("5:20"),SizedBox(height: 6),CircleAvatar(child: Text("4"),backgroundColor: Colors.pink,radius: 10,)],),
+                   ),
+          ),
+        
+        
+       ]
+        
      ) ,
-     backgroundColor: Colors.white,
+    
 
     );
   }
